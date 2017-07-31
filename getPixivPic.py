6ade0574fd8c27fa9bd593a0f79d9dc8c239a2d6
MowenPan <pan19950817@hotmail.com>
2017-07-31 20:40:09 +0800

    打包程序

--- getPixivPic.py
+++ getPixivPic.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import msvcrt
 import requests
 from pixivpy3 import AppPixivAPI, PixivError
 
@@ -36,12 +37,35 @@ def get_artist_url(artist_id):
     return url
 
 
+def pwd_input():
+    chars = []
+    while True:
+        try:
+            newChar = msvcrt.getch().decode(encoding="utf-8")
+        except:
+            return input("你很可能不是在cmd命令行下运行，密码输入将不能隐藏:")
+        if newChar in '\r\n':  # 如果是换行，则输入结束
+             break
+        elif newChar == '\b':  # 如果是退格，则删除密码末尾一位并且删除一个星号
+             if chars:
+                 del chars[-1]
+                 msvcrt.putch('\b'.encode(encoding='utf-8'))  # 光标回退一格
+                 msvcrt.putch( ' '.encode(encoding='utf-8'))  # 输出一个空格覆盖原来的星号
+                 msvcrt.putch('\b'.encode(encoding='utf-8'))  # 光标回退一格准备接受新的输入
+        else:
+            chars.append(newChar)
+            msvcrt.putch('*'.encode(encoding='utf-8')) # 显示为星号
+    return ''.join(chars)
+
+
 def check_login():
     username = input('请输入你的P站账户名或者邮箱：\n')
-    password = input('请输入你的P站账户密码：\n')
+    print('请输入你的P站账户密码：')
+    password = pwd_input()
+    # password = input('请输入你的P站账户密码：\n')
     try:
         if aapi.login(username=username, password=password):
-            print('    [*]登陆成功！！！')
+            print('\n    [*]登陆成功！！！')
     except PixivError:
         print('    [!]请检查账户名和密码！！！')
         check_login()
